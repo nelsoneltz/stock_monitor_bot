540817316f1efb6de0fe8fffc79e65a06e18a2c0
Nelson Eltz <nelsonfse@gmail.com>
2026-04-10 10:59:06 -0300

    feat: main now get tickets from stock_threshold.jsonn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from api import get_stock_price
 from charts import create_charts
@@ -7,33 +8,48 @@ from notifier import send_discord_message
 # Load environment variables
 load_dotenv()
 
+
 def main():
     # Ensure the working directory is the script's directory
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
-    tickers = ["AURA33", "PETR4", "VALE3"]
+
+    # Load tickers from stock_threshholds.json
+    with open("stock_threshholds.json", "r") as f:
+        thresholds = json.load(f)
+    tickers = list(thresholds.keys())
+
     stock_results = []
 
     print("Starting data collection...")
     for ticker in tickers:
         try:
             price, change, time, res = get_stock_price(ticker)
-            stock_results.append({
-                'ticker': ticker, 'price': price, 
-                'change': change, 'time': time, 'stock_data': res
-            })
+            stock_results.append(
+                {
+                    "ticker": ticker,
+                    "price": price,
+                    "change": change,
+                    "time": time,
+                    "stock_data": res,
+                }
+            )
         except Exception as e:
             print(f"Error processing {ticker}: {e}")
-    
+
     print("Generating charts...")
-    create_charts() 
+    create_charts()
 
     print("Sending notifications...")
     for stock in stock_results:
         send_discord_message(
-            stock['price'], stock['change'], 
-            stock['time'], stock['ticker'], stock['stock_data']
+            stock["price"],
+            stock["change"],
+            stock["time"],
+            stock["ticker"],
+            stock["stock_data"],
         )
+
 
 if __name__ == "__main__":
     main()
+
